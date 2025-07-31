036f6b7e1c509d3b02bb8d072c08ab66ee9e8ce2
Nikozszs <06@yandex.ru>
2025-07-31 15:59:35 +0300

    12 sprint

---
+++ src/components/__tests__/reducers/rootReducer.test.ts
@@ -0,0 +1,36 @@
+import { 
+    initialState as ingredientsInitialState
+} from "../../services/slices/ingredientSlice";
+import { 
+    initialState as constructorInitialState 
+} from "../../services/slices/constructorItemsSlice";
+import { 
+    initialState as orderInitialState 
+} from "../../services/slices/orderSlice";
+import { 
+    initialState as userInitialState 
+} from "../../services/slices/userSlice";
+import { rootReducer, RootState } from "../../services/store"
+
+describe('rootReducer', () => {
+    test('должен возвращать начальное состояние при вызове с неопределенным состоянием и неизвестным действием', () => {
+        const expectInitialState: RootState  = {
+            ingredients: ingredientsInitialState,
+            constructorItems: constructorInitialState,
+            order: orderInitialState,
+            user: userInitialState
+        };
+        const result = rootReducer(undefined, { type: 'UNKNOWN_ACTION' });
+        expect(result).toEqual(expectInitialState)
+    })
+    test('не должен изменять состояние при неизвестном действии', () => {
+        const testState = {
+            ingredients: ingredientsInitialState,
+            constructorItems: constructorInitialState,
+            order: orderInitialState,
+            user: userInitialState
+        };
+        const result = rootReducer(testState, { type: 'UNKNOWN_ACTION' });
+        expect(result).toBe(testState)
+    })
+})
