813f653dee0143d9d7f22813d2f37caa02d4d531
Nikozszs <06@yandex.ru>
2025-07-31 14:55:49 +0300

    cypress UPD

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,8 +1,3 @@
-Cypress.Commands.add('login', () => {
-    cy.setCookie('accessToken', 'fake-token');
-    window.localStorage.setItem('refreshToken', 'fake-refresh-token');
-});
-
 Cypress.Commands.add('addIngredient', (selector) => {
     return cy.get(selector).first().click();
 });
